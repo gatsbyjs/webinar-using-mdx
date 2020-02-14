76c84a41138bf6aa4e113d006b46c8d550546674
Kyle Gill <kylerobertgill@gmail.com>
2020-02-14 11:10:38 -0700

    add custom form component and include on the page

---
+++ src/components/form.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Form = () => {
+  return (
+    <form>
+      <label for="email">Email:</label>
+      <input type="email" name="email" />
+      <input type="submit" value="submit" />
+    </form>
+  )
+}
+
+export default Form
